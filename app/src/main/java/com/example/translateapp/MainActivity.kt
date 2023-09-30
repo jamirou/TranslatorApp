abb0ef7f61d6028074024d0ee0bd8d720cb3bad8
Jamiro <jamirocristian15@gmail.com>
2023-09-30 15:30:41 -0300

    first commit

---
+++ app/src/main/java/com/example/translateapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.translateapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.translateapp.ui.theme.TranslateAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TranslateAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello Android", fontWeight = FontWeight.Bold)
+        Text(text = "The world is yours")
+    }
+}
+
+
+
+
+
+
+
