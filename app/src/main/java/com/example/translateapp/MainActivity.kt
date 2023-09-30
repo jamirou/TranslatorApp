067297211f9e2502eb7074f8c67b2d6dfeb64407
Jamiro <jamirocristian15@gmail.com>
2023-09-30 15:40:27 -0300

    added strgins forlder

--- app/src/main/java/com/example/translateapp/MainActivity.kt
+++ app/src/main/java/com/example/translateapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.translateapp.ui.theme.TranslateAppTheme
@@ -41,8 +42,8 @@ fun MyView() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Hello Android", fontWeight = FontWeight.Bold)
-        Text(text = "The world is yours")
+        Text(text = stringResource(id = R.string.title), fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.subtitle))
     }
 }
 
