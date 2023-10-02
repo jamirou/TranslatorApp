da00604e211b7fe18fe3f4b92965486ad109e8b5
Jamiro <jamirocristian15@gmail.com>
2023-10-02 15:41:09 -0300

    complete structure and view

--- app/src/main/java/com/example/translateapp/translator/TranslateView.kt
+++ app/src/main/java/com/example/translateapp/translator/TranslateView.kt
@@ -2,11 +2,22 @@ package com.example.translateapp.translator
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -16,11 +27,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun TranslateView(viewModel: TranslateViewModel) {
     val state = viewModel.state
@@ -74,6 +87,50 @@ fun TranslateView(viewModel: TranslateViewModel) {
                 }
             )
         }
+
+        Spacer(modifier = Modifier.height(15.dp))
+        OutlinedTextField(
+            value = state.textToTranslate,
+            onValueChange = { viewModel.onValue(it) },
+            label = { Text(text = "Write...") },
+            KeyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            KeyboardActions = KeyboardActions(
+                onDone = {
+                    viewModel.onTranslate(
+                        state.textToTranslate,
+                        context,
+                        selectedSourceLang,
+                        selectedTargetLang
+                    )
+                }
+            ),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Transparent,
+                unfocusedBorderColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        if (state.isDownloading) {
+            CircularProgressIndicator()
+            Text(text = "Downloading model...")
+        } else {
+            OutlinedTextField(
+                value = state.translateText,
+                onValueChange = {},
+                label = { Text(text = "") },
+                readOnly = false,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                )
+        }
+
 
     }
 }
