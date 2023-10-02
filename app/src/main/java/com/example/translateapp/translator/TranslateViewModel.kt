a15a4a7d0565f0d8d930880b54a4f545d8a72b5d
Jamiro <jamirocristian15@gmail.com>
2023-10-02 14:02:07 -0300

    translating text pt2

--- app/src/main/java/com/example/translateapp/translator/TranslateViewModel.kt
+++ app/src/main/java/com/example/translateapp/translator/TranslateViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.translateapp.translator
 
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
 
 class TranslateViewModel : ViewModel() {
@@ -31,9 +34,28 @@ class TranslateViewModel : ViewModel() {
                     translateText = translateText
                 )
             }.addOnFailureListener {
-
+                Toast.makeText(context, "Downloading model...", Toast.LENGTH_SHORT).show()
+                downloadingModel(languageTranslator, context)
             }
-        
+    }
+    private fun downloadingModel(languageTranslator: Translator, context: Context) {
+        state = state.copy(
+            isDownloading = true
+        )
+        val conditions = DownloadConditions
+            .Builder()
+            .requireWifi()
+            .build()
+        languageTranslator
+            .downloadModelIfNeeded()
+            .addOnSuccessListener {
+                Toast.makeText(context, "Model downloaded", Toast.LENGTH_SHORT).show()
+                state = state.copy(
+                    isDownloading = false
+                )
+            }.addOnFailureListener {
+                Toast.makeText(context, "Failed model downloaded", Toast.LENGTH_SHORT).show()
+            }
     }
 }
 
